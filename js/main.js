732926a8cffedda64b61101888e2371dee05cb4b
Delta <60229705+Dr-Alex-Dr@users.noreply.github.com>
2021-02-12 14:34:00 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let btn = document.getElementsByClassName('icon-bars')[0];
+let phone_menu = document.getElementsByClassName('menu__list')[0];
+let index = 0;
+
+btn.onclick = function(){
+	if (index == 0){
+		phone_menu.style.display = "block";
+		index += 1;
+	}
+	else{
+		phone_menu.style.display = "none";
+		index -= 1;
+	}
+}
+
+$(function(){
+
+    $('.reviews__slider').slick({
+		prevArrow: '<button class="slider__btn slider__btn-left"></button>',
+        nextArrow: '<button class="slider__btn slider__btn-right"></button>',
+		slidesToShow: 3,
+		slidesToScroll: 1
+    });
+
+	$('.questions__item-title').on('click', function(){
+        $('.questions__item').removeClass('questions__item--active');
+        $(this).parent().addClass('questions__item--active');
+    });
+});
+
